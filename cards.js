80ae083541cc4ae6db4791891c267566ba1d9326
sebastian Ruiz <sruiz5260@gmail.com>
2017-08-09 12:17:32 -0700

    logic for deck shuffling

--- cards.js
+++ cards.js
@@ -3,11 +3,43 @@ var cards = [
 '2s','3s','4s','5s','6s','7s','8s','9s','10s','js','qs','ks', 'as',
 '2c','3c','4c','5c','6c','7c','8c','9c','10c','jc','qc','kc', 'ac',
 '2d','3d','4d','5d','6d','7d','8d','9d','10d','jd','qd','kd', 'ad',
-]
+];
 
-function drawCard() {
-  drawnCards.push(cards.random(0, 51));
+var deck = []; //should be a shuffled deck
+
+//returns true is the card drawn is in the deck, false if it's not
+function alreadyThere(value){
+  if (deck.length == 0) return false;
+
+  for (var i = 0; i < deck.length; i++){
+    if (deck[i] == value) return true;
+  }
+
+  return false;
 }
-var drawnCards = []
 
-while drawnCards.push(cards.random);
+//this draw a random card and returns a unique card
+function draw(){
+  var card = cards[Math.floor(Math.random() * cards.length)];
+
+  //until we find a card that's unique
+  while (alreadyThere(card)){
+    card = cards[Math.floor(Math.random() * cards.length)];
+  }
+
+  return card;
+}
+
+//in this function: get random cards and put them into deck
+function shuffle(){
+  while (deck.length != 52){
+    deck.push(draw());
+  }
+}
+
+
+function printDeck(){
+  for (var i = 0; i < 52; i++){
+    console.log(deck[i]);
+  }
+}
